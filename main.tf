1318841b8249387eda97fababf6259562ae7eaa0
Chido24 <chidozie.unachukwu@megaport.com>
2024-11-29 17:38:21 -0800

    Load balancer modify target group

--- main.tf
+++ main.tf
@@ -58,25 +58,22 @@ module "alb" {
   # Security Group
   security_groups = [module.blog_sg.security_group_id]
 
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
 
-  listeners = {
-    
-    ex-http = {
-      port            = 80
-      protocol        = "HTTP"
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
       target_group_index = 0
     }
-  }
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
+  ]
 
   tags = {
     Environment = "dev"
