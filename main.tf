e4cf95b3236b4cd2da0e4bc9ab89ca47e3070b17
Chido24 <chidozie.unachukwu@megaport.com>
2024-11-29 21:55:01 -0800

    Autoscaling group 4th modification

--- main.tf
+++ main.tf
@@ -51,35 +51,33 @@ module "blog_autoscaling" {
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
+  name = "blog-alb"
 
-  # Security Group
-  security_groups = [module.blog_sg.security_group_id]
+  load_balancer_type = "application"
 
-  target_groups   = {
-    blog-instance = {
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
       name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      target_id        = module.autoscaling.blog.id
     }
-  }
+  ]
 
-  listeners = {
-    http_tcp_listeners = {
-      port       = 80
-      protocol   = "HTTP"
-
-      forward = {
-      target_group_key = "blog-instance"
-      }
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
     }
-  }
+  ]
 
   tags = {
     Environment = "dev"
