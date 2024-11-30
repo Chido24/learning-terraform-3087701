1966d8e5cefd07c16bd36b765688c2910ac575b5
Chido24 <chidozie.unachukwu@megaport.com>
2024-11-29 20:59:49 -0800

    autoscaling

--- main.tf
+++ main.tf
@@ -35,20 +35,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.0.0"
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  name     = "blog"
+  min_size = 1
+  max_size = 2
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  vpc_zone_identifier  = module.blog_vpc.public_subnets
+  target_group_arns    = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  image_id          = data.aws_ami.app_ami.id
+  instance_type     = var.instance_type
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
@@ -64,7 +67,7 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      target_id        = module.autoscaling.blog.id
     }
   }
 
